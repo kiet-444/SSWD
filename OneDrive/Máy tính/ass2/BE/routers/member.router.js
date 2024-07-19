7514b94848984d05e93e6a3c45e5f4a16cc1019f
Kiet Anh <70312316+kiet-444@users.noreply.github.com>
2024-07-19 11:34:33 +0700

    Delete OneDrive/Máy tính/ass2 directory

--- OneDrive/Máy tính/ass2/BE/routers/member.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const MemberController = require("../controllers/member.controller")
-const jwtAuthentication = require('../middlewares/jwtAuthentication');
-
-router.route("/getMembers").get(jwtAuthentication.verifyToken, MemberController.getMembers)
-
-module.exports = router
--- OneDrive/Máy tính/ass2/FE/src/pages/Admin/Watch/MakeData.js
+++
@@ -1,4 +0,0 @@
-export const automaticData = [
-    'True',
-    'False',
-];
