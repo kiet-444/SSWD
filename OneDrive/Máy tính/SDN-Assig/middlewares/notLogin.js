c49a8ab50a04f95e0f24b692bed17aeafe4eee6d
Kiet Anh <70312316+kiet-444@users.noreply.github.com>
2024-07-19 11:33:42 +0700

    Delete OneDrive/Máy tính/SDN-Assig directory

--- OneDrive/Máy tính/SDN-Assig/middlewares/notLogin.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.session.memberId) {
-    return res.redirect("/");
-  }
-  next();
-};
--- OneDrive/Máy tính/SDN-Assig/routers/member.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const MemberController = require("../controllers/member.controller")
-const admin = require("../middlewares/verify")
-
-router.get('/accounts', admin, MemberController.getAccount)
-
-module.exports = router
